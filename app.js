37a1d626ec769efb8dbdebc1131b18bed319f06b
jeska706 <jessicabeardsley@hotmail.com>
2016-11-22 18:50:19 -0700

    Commit 8 : latest work

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ console.log("Let's play! apps.js is connected");
 $(function(){
 //grab gameBoard
 var $gameBoard = $("#game-board");
-var $playerTurn = "red";
-var $posPositions = $(".possible-positions");
+var $player = "red";
+var $positions = $(".positions");
 
 //start button
 var $startBtn = $("#start-game");
@@ -17,16 +17,16 @@ var $startBtn = $("#start-game");
 // rest button
 var $resetBtn = $("#reset");
   $resetBtn.on("click", function(){
-    $posPositions.css("background-color", "white");
+    $positions.css("background-color", "white");
     console.log("reset button clicked");
     alert("Click start for a new game");
 });
 
 //on click function
 var $play = function(){
-    $posPositions.on('click', $checkedClicked);
+    $positions.on('click', $checkedClicked);
     console.log("this is the play function");
-    
+
 };
 
 //checks if the space has been clicked
@@ -34,29 +34,32 @@ var $checkedClicked = function(){
   // console.log("this is the checkedClicked function");
   var $theClass = $(this).attr("class");
   var $that = $(this);
-      // console.log($theClass + " this is the class");
-      if ($theClass === "possible-positions clicked"){
-        // console.log("this is the if statement");
+  console.log($that);
+      // console.log($that);
+      if ($theClass === "positions clicked"){
         // alert("This space is taken, please select another");
       } else {
         $(this).addClass("clicked");
         // $(this).attr("played", "true");
-        // console.log("this is the else statement");
         $assignSpace($that);
       }
     }
 
 var $assignSpace = function($that){
   // console.log("this is the assignSpace function");
-      if ($playerTurn === "red"){
+  var $win = $(this);
+  // console.log($win);
+      if ($player === "red"){
         console.log("Hey red");
         $that.addClass("red");
-        $playerTurn = "yellow";
+        $player = "yellow";
       } else {
         console.log("Hey yellow");
         $that.addClass("yellow");
-        $playerTurn = "red";
+        $player = "red";
        }
+       $winner($that);
+
      }
 
 
@@ -78,35 +81,35 @@ var $O = $("#O");
 var $P = $("#P");
 
 //winning conditions
-var $winner = function($that){
-  if ($that === "possible-positions clicked red"
-          && $A === $B && $C === $D ||
-             $A === $E && $I === $M ||
-             $A === $F && $K === $P ||
-             $D === $H && $L === $P ||
-             $D === $G && $J === $M ||
-             $M === $N && $O === $P ||
-             $E === $F && $G === $H ||
-             $I === $J && $K === $L ||
-             $B === $F && $J === $N ||
-             $C === $G && $K === $O){
+var $winner = function($win){
+  console.log("this is the winner function"+ $win);
+  if (($win == "positions.clicked.red")
+          && (($A === $B && $C === $D) ||
+             ($A === $E && $I === $M) ||
+             ($A === $F && $K === $P) ||
+             ($D === $H && $L === $P) ||
+             ($D === $G && $J === $M) ||
+             ($M === $N && $O === $P) ||
+             ($E === $F && $G === $H) ||
+             ($I === $J && $K === $L) ||
+             ($B === $F && $J === $N) ||
+             ($C === $G && $K === $O))){
             alert("Red is the winner");
-          } else if ($that === "possible-positions clicked yellow"
-            && $A === $B && $C === $D ||
-               $A === $E && $I === $M ||
-               $A === $F && $K === $P ||
-               $D === $H && $L === $P ||
-               $D === $G && $J === $M ||
-               $M === $N && $O === $P ||
-               $E === $F && $G === $H ||
-               $I === $J && $K === $L ||
-               $B === $F && $J === $N ||
-               $C === $G && $K === $O){
+          } else if (($win == "positions.clicked.yellow")
+            && ($A === $B && $C === $D )||
+               ($A === $E && $I === $M) ||
+               ($A === $F && $K === $P) ||
+               ($D === $H && $L === $P) ||
+               ($D === $G && $J === $M) ||
+               ($M === $N && $O === $P) ||
+               ($E === $F && $G === $H) ||
+               ($I === $J && $K === $L) ||
+               ($B === $F && $J === $N )||
+               ($C === $G && $K === $O)){
               alert("Yellow is the winner");
          } else {
               console.log("Next player moves");
         }
       }
-  $winner();
 
 });
